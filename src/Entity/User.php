2f2febdd43ae05321145b2ef5042b8dac42d307d
jokerov <jokerov@gmail.com>
2023-06-08 09:03:23 +0300

    Create api for user registration

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,24 +2,60 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Post;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints\Email;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ApiResource(
+    operations: [
+        new Post()
+    ],
+    normalizationContext: ['groups' => ['users:read']],
+    denormalizationContext: ['groups' => ['users:write']],
+)]
+#[UniqueEntity('email')]
 class User
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['users:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Email(
+        mode: Email::VALIDATION_MODE_STRICT
+    )]
+    #[Assert\Length(
+        min: 5,
+        max: 255
+    )]
+    #[Groups(['users:write'])]
     private ?string $email = null;
 
     #[ORM\Column(length: 100)]
+    #[Assert\NotBlank]
+    #[Assert\Length(
+        min: 1,
+        max: 100
+    )]
+    #[Groups(['users:write'])]
     private ?string $firstName = null;
 
     #[ORM\Column(length: 100)]
+    #[Assert\NotBlank]
+    #[Assert\Length(
+        min: 1,
+        max: 100
+    )]
+    #[Groups(['users:write'])]
     private ?string $lastName = null;
 
     public function getId(): ?int
